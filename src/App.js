d63da96075cd4d2eb908fb09bb828c1bd2bcd9b7
biondiylun <biondilun@hotmail.com>
2020-02-10 20:07:52 -0800

    In progress calculator buttons

--- src/App.js
+++ src/App.js
@@ -3,21 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const number = 10;
+  function increment() {
+    number++
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{number}</p>
+        <button onclick="increment()">ABC</button>
       </header>
     </div>
   );
---
+++ src/CalcButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CalcButton(props) {
+    return (        
+    <div className="calcbutton">
+        <button> value ={props.value}</button>
+    </div>
+    )
+}
